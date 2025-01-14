c9d796ab81ddacb90777e483cefa29373100366b
Virochana <gmvirochan@gmail.com>
2025-01-14 16:04:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -242,5 +242,6 @@ def student():
 
     return render_template('student.html', message=message, message_type=message_type)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from the environment or default to 5000
+    app.run(host="0.0.0.0", port=port)
